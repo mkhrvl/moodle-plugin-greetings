63908290cad431054e2067d7c0b2083853b5cdc8
mkhrvl <kenrevil.10@gmail.com>
2022-08-22 13:10:12 +0800

    Fixed line formatting in index.php

--- index.php
+++ index.php
@@ -26,13 +26,10 @@ require_once('../../config.php');
 require_once($CFG->dirroot. '/local/greetings/lib.php');
 
 $context = context_system::instance();
-
 $PAGE->set_context($context);
 $PAGE->set_url(new moodle_url('/local/greetings/index.php'));
 $PAGE->set_pagelayout('standard');
-
 $PAGE->set_title($SITE->fullname);
-
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 echo $OUTPUT->header();
