f308a9fcff2aeb98add52031f187301aeb420fe2
mkhrvl <kenrevil.10@gmail.com>
2022-08-22 23:27:19 +0800

    Save and display greeting messages

--- index.php
+++ index.php
@@ -35,6 +35,18 @@ $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 $messageform = new local_greetings_message_form();
 
+if ($data = $messageform->get_data()) {
+    $message = required_param('message', PARAM_TEXT);
+
+    if (!empty($message)) {
+        $record = new stdClass;
+        $record->message = $message;
+        $record->timecreated = time();
+
+        $DB->insert_record('local_greetings_messages', $record);
+    }
+}
+
 echo $OUTPUT->header();
 
 if (isloggedin()) {
@@ -45,10 +57,21 @@ if (isloggedin()) {
 
 $messageform->display();
 
-if ($data = $messageform->get_data()) {
-    $message = required_param('message', PARAM_TEXT);
+$messages = $DB->get_records('local_greetings_messages');
 
-    echo $OUTPUT->heading($message, 4);
+echo $OUTPUT->box_start('card-columns');
+
+foreach ($messages as $m) {
+    echo html_writer::start_tag('div', array('class' => 'card'));
+    echo html_writer::start_tag('div', array('class' => 'card-body'));
+    echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::start_tag('p', array('class' => 'card-text'));
+    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
+    echo html_writer::end_tag('p');
+    echo html_writer::end_tag('div');
+    echo html_writer::end_tag('div');
 }
+
+echo $OUTPUT->box_end();
 
 echo $OUTPUT->footer();
